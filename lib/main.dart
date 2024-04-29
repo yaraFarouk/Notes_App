2d4f5b141d6b789916fc23f3383651c6279a2d74
yaraFarouk <yaraFarouk960@gmail.com>
2024-04-29 17:17:55 +0300

    change theme and create notes view

--- lib/main.dart
+++ lib/main.dart
@@ -4,31 +4,13 @@ void main() {
   runApp(const NotesApp());
 }
 
-class NotesApp extends StatefulWidget {
+class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
-
-  @override
-  State<NotesApp> createState() => _NotesAppState();
-}
-
-class _NotesAppState extends State<NotesApp>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(),
+    )
   }
 }
